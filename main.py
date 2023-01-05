3edcd8da32d193c784d63457df92eed037ee6f3f
Spencer Wozniak <98838077+spencerwozniak@users.noreply.github.com>
2023-01-05 18:58:59 -0500

    FishBot v2

--- main.py
+++ main.py
@@ -12,12 +12,13 @@ import time as t
 import random as r
 from PIL import ImageGrab as g
 
+# Function for mouse movement
 def move_mouse(dt=0.2):
-    m.moveRel(100,100,duration=dt/2)
-    m.moveRel(-100,-100,duration=dt/2)
+    m.moveRel(100,100,duration=dt/2) # Moves mouse away from original position
+    m.moveRel(-100,-100,duration=dt/2) # Moves mouse back to original position
 
+# Function for random character movement
 def random_movement(a=0.05,b=0.1):
-
     keys = ['w','a','s','d']
     r.shuffle(keys) # Shuffles WASD key presses
 
@@ -28,8 +29,8 @@ def random_movement(a=0.05,b=0.1):
     for key in keys:
         m.keyUp(key) # Key release
 
+# Function for fixed character movement
 def fixed_movement(dt=0.05):
-
     keys = ['w','a','s','d']
 
     m.keyUp('shift')
@@ -42,10 +43,11 @@ def fixed_movement(dt=0.05):
 
     t.sleep(0.05)
     m.keyDown('shift')
-        
-def show_inventory(time=1.0):
+
+# Function that shows inventory
+def show_inventory(dt=1.0):
     m.press('e')
-    t.sleep(time)
+    t.sleep(dt)
     m.press('esc')
 
     m.keyUp('shift')
@@ -77,13 +79,15 @@ def main():
         t.sleep(r.random()*0.25) # Random delay
         m.click(button='right') # Reel rod in
         
-        move_mouse(0.2)
+        move_mouse(0.2) # Move mouse for 200 ms
         random_movement(a=0.02,b=0.02) # Move around randomly
-        show_inventory(time=0.1)
+        show_inventory(0.1) # Show inventory for 100 ms
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
+        t.sleep(0.05)
         m.keyUp('shift')
+        t.sleep(0.05)
         sys.exit(0)
